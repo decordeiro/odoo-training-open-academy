65902aef64316432cf759b1164b253bff113a064
André Augusto <a.cordeito@gmail.com>
2016-03-30 13:54:19 -0300

    [NEW] Adicionandp arquivos base do módulo

---
+++ open_academy/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 - ANDRE AUGUSTO - YOU BY PICCADILLY
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "OpenAcademy",
+    "summary": "Modulo de teste do open academy",
+    "version": "8.0.1.0.0",
+    "category": "Custom",
+    "website": "https://www.youbypiccadilly.com.br",
+    "author": "ANDRE, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
